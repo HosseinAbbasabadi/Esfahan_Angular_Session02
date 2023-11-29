1b8fbc670c5ffc924f9dac25ac518bbc5adb60d7
Hossein <Hossein.Abbasabadi@outlook.com>
2023-11-29 11:35:04 +0330

    ng model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,71 @@
 import { Component } from '@angular/core';
 import { Task } from './Task';
-import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { NgClass, NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault],
+  imports: [
+    NgFor,
+    NgIf,
+    NgSwitch,
+    NgSwitchCase,
+    NgSwitchDefault,
+    NgClass,
+    FormsModule],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'todo-list'
   // tasks1 = new Array<Task>()
   tasks: Task[] = []
+  taskTitle: string = ''
 
   constructor() {
-    this.tasks = [
-      {
-        id: 1,
-        title: 'Wake Up At 07:00',
-        status: 3
-      },
-      {
-        id: 2,
-        title: 'Esfahan Angular Course Session 03',
-        status: 2
-      },
-      {
-        id: 3,
-        title: 'Complete HRM Project',
-        status: 1
-      },
-      {
-        id: 4,
-        title: 'PIS Meeting',
-        status: 1
-      },
-    ]
+  }
+
+  addTask() {
+    if (!this.taskTitle) {
+      alert('Title cant be empty.')
+      return
+    }
+
+    if (this.tasks.find(x => x.title == this.taskTitle)) {
+      alert('Task already defined.')
+      return
+    }
+
+    const task: Task = {
+      id: this.tasks.length + 1,
+      title: this.taskTitle,
+      status: 1
+    }
+
+    this.tasks.push(task)
+    this.taskTitle = ''
+  }
+
+  titleChanged() {
+    console.log(this.taskTitle)
+  }
+
+  changeStatus(taskId: number, status: number) {
+    const task = this.tasks.find(x => x.id == taskId)!
+    task.status = status
+  }
+
+  deleteTask(taskId: number) {
+    const task = this.tasks.find(x => x.id == taskId)!
+    if (confirm(`Do you want to delete ${task.title}`) == true) {
+      const index = this.tasks.indexOf(task)
+      this.tasks.splice(index, 1)
+    }
+  }
+
+  deleteAll() {
+    if (!this.tasks.length) return
+
+    this.tasks = []
   }
 }
