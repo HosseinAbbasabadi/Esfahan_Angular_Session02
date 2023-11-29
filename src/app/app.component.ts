8e11dd6cdc5fc1a95d6d25ce913c813e8f0f5070
Hossein <Hossein.Abbasabadi@outlook.com>
2023-11-29 09:55:12 +0330

    ngfor, ngif, ngswith

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
+import { Task } from './Task';
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'todo-list'
+  // tasks1 = new Array<Task>()
+  tasks: Task[] = []
+
+  constructor() {
+    this.tasks = [
+      {
+        id: 1,
+        title: 'Wake Up At 07:00',
+        status: 3
+      },
+      {
+        id: 2,
+        title: 'Esfahan Angular Course Session 03',
+        status: 2
+      },
+      {
+        id: 3,
+        title: 'Complete HRM Project',
+        status: 1
+      },
+      {
+        id: 4,
+        title: 'PIS Meeting',
+        status: 1
+      },
+    ]
+  }
 }
